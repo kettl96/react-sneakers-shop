9d50e1f481f04548481989ae5398459847433103
kettl96 <kettl110e4@gmail.com>
2022-05-29 05:14:01 +0300

    feat: add props to card

--- sneakers-shop/src/App.js
+++ sneakers-shop/src/App.js
@@ -1,18 +1,23 @@
 import c from './Content.module.css'
 
 import search from './assets/search.svg'
-import removeBtn from './assets/removeBtn.svg'
-import nextArrow from './assets/nextArrow.svg'
 
 import one from './assets/sneakers/1.jpg'
-
-
-
+import two from './assets/sneakers/2.jpg'
+import three from './assets/sneakers/3.jpg'
+import four from './assets/sneakers/4.jpg'
 
 
 import Card from './components/card/Card'
 import Header from './components/header/Header'
 import Cart from './components/cart/Cart';
+
+const cardInfoArr = [
+  { name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 122, img: one },
+  { name: 'Мужские Кроссовки Nike Air Max 270', price: 210, img: two },
+  { name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 99, img: three },
+  { name: 'Кроссовки Puma X Aka Boku Future Rider', price: 199, img: four },
+]
 
 function App() {
   return (
@@ -28,11 +33,12 @@ function App() {
           </div>
         </div>
         <div className={c.card__wrapper}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {cardInfoArr.map(item => {
+            return <Card
+              name={item.name}
+              price={item.price}
+              img={item.img}  />
+          })}
         </div>
 
       </div>
--- sneakers-shop/src/components/card/Card.jsx
+++ sneakers-shop/src/components/card/Card.jsx
@@ -1,24 +1,29 @@
-import c from './Content.module.css'
+import c from './Card.module.css'
 import plus from '../../assets/plus.svg'
 
 import one from '../../assets/sneakers/1.jpg'
+import added from '../../assets/added.svg'
 
-function Card() {
+const addClick = (item) => {
+  return item.target.src = added
+}
+
+function Card(props) {
   return (
     <div className={c.card}>
-            <div className={c.add__favorite}></div>
-            <img src={one} alt="" className={c.sneaker__img} />
-            <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
-            <div className={c.card__info}>
-              <div>
-                <span>Price:</span>
-                <b>122 $</b>
-              </div>
-              <button>
-                <img src={plus} alt="" />
-              </button>
-            </div>
-          </div>
+      <div className={c.add__favorite}></div>
+      <img src={props.img} alt="" className={c.sneaker__img} />
+      <p>{props.name}</p>
+      <div className={c.card__info}>
+        <div>
+          <span>Price:</span>
+          <b>{props.price} $</b>
+        </div>
+        <button onClick={addClick}>
+          <img src={plus} alt="" />
+        </button>
+      </div>
+    </div>
   )
 }
 
